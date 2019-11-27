5b1e63b21d53231a26cfaaa387bcaa73247cd6d1
Spy C <58186262+AnthonyPerugini@users.noreply.github.com>
2019-11-27 16:45:46 -0500

    Add files via upload

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -2,7 +2,19 @@ from random import randint
 
 player_BO3_score = 0
 computer_BO3_score = 0
+
+
+def report_outcome(winner):
+    if winner == "player":
+        print("Computer loses to your " + str(player_choice) + " with their " + str(computer_choice))
+        print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+    else:
+        print("Computer beats your " + str(player_choice) + " with their " + str(computer_choice))
+        print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+
 # Have computer make a random roll from the array of options
+
+
 computer_possible_choices = ["Rock", "Paper", "Scissors"]
 computer_choice = (computer_possible_choices[randint(0, 2)])
 # Run game until one player reaches 3 points
@@ -11,45 +23,36 @@ while computer_BO3_score < 3 and player_BO3_score < 3:
     player_choice = input("Choose one: Rock, Paper, or Scissors: ")
     print("You guessed: " + str(player_choice))
     print("Computer guessed: " + str(computer_choice))
-    # Compare all possible outcomes from players and computers choices and add scores to BO3 total
+# Compare all possible outcomes from players and computers choices and add scores to BO3 total
     if player_choice == computer_choice:
         print("You two mofuckas tied!  Play again.")
     elif player_choice == "Rock":
         if computer_choice == "Paper":
-            print("Computer beats your " + str(player_choice) + " with their " + str(computer_choice))
             computer_BO3_score += 1
-            print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+            report_outcome("computer")
         else:
-            print("Computer loses to your " + str(player_choice) + " with their " + str(computer_choice))
             player_BO3_score += 1
-            print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+            report_outcome("player")
     elif player_choice == "Paper":
         if computer_choice == "Scissors":
-            print("Computer beats your " + str(player_choice) + " with their " + str(computer_choice))
             computer_BO3_score += 1
-            print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+            report_outcome("computer")
         else:
-            print("Computer loses to your " + str(player_choice) + " with their " + str(computer_choice))
             player_BO3_score += 1
-            print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+            report_outcome("player")
     elif player_choice == "Scissors":
         if computer_choice == "Rock":
-            print("Computer beats your " + str(player_choice) + " with their " + str(computer_choice))
             computer_BO3_score += 1
-            print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+            report_outcome("computer")
         else:
-            print("Computer loses to your " + str(player_choice) + " with their " + str(computer_choice))
             player_BO3_score += 1
-            print("Computers Score: " + str(computer_BO3_score) + "\nPlayers Score: " + str(player_BO3_score))
+            report_outcome("player")
     else:
         print("you did not choose a valid move, try dat shit again.")
-    # Reset the computers RPS choice
+# Reset the computers RPS choice
     computer_choice = (computer_possible_choices[randint(0, 2)])
-
+# Show BO3 winner
 if player_BO3_score == 3:
     print("Oh damn, you won the game!  Aint dat some shit.")
 else:
-    print("You lost the game?  Try again like yo momma taught you right.")
-
-# Print out results of the game to console
-# Reset the computers choice to another random selection
+    print("You lost the game?  Try again like your momma taught you right.")
